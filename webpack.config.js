77504c8366eadbad4971ef62f1c83ddf62cfe49c
Darin Acosta <darinjacosta@gmail.com>
2017-06-27 16:42:19 -0500

    init less compiling

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,35 @@
 var webpack = require('webpack');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
     context: __dirname + '/app',
+    module: {
+        loaders: [
+          {
+             test: /\.css$/,
+             loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader' })
+           },
+           {
+             test: /\.less$/,
+             loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader!less-loader' })
+           }
+        ]
+    },
     entry: {
         app: './app.js',
+        style: '../style/style.js',
         vendor: ['angular']
     },
     output: {
         path: __dirname + '/js',
-        filename: 'app.bundle.js'
+        filename: '[name].bundle.js'
     },
     devServer: {
       compress: true,
       port: 9090
     },
     plugins: [
-        new webpack.optimize.CommonsChunkPlugin({ name: 'vendor', filename: 'vendor.bundle.js' })
+        new webpack.optimize.CommonsChunkPlugin({ name: 'vendor', filename: 'vendor.bundle.js' }),
+        new ExtractTextPlugin("../style/css/bundle.css")
     ]
 };
