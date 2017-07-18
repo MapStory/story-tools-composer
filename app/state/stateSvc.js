73c2678ce27a4fcec91396b2fa535a55aec12239
Darin Acosta <darinjacosta@gmail.com>
2017-07-18 16:39:59 -0500

    update templating

--- app/state/stateSvc.js
+++ app/state/stateSvc.js
@@ -1,18 +1,23 @@
 function stateSvc($location, $q) {
   var svc = {};
+  svc.config = null;
   svc.getConfig = function () {
     var deferred = $q.defer();
     var path = $location.path();
     var mapID = /\/maps\/(\d+)/.exec(path) ? /\/maps\/(\d+)/.exec(path)[1] : null;
     var mapJsonUrl = '/maps/' + mapID + '/data';
-    if (mapID) {
+    if (svc.config) {
+      deferred.resolve(svc.config);
+    } else if (mapID) {
       $.ajax({
         dataType: "json",
         url: mapJsonUrl ,
         }).done(function ( data ) {
+          svc.config = data;
           deferred.resolve(data);
       });
     } else {
+      svc.config = window.config;
       deferred.resolve(window.config);
     }
     return deferred.promise;
