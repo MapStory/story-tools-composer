77e4e6022e62017e307a63906221dae914b739ce
Darin Acosta <darinjacosta@gmail.com>
2017-07-31 15:59:10 -0500

    update mapManager tests

--- app/map/mapManager.js
+++ app/map/mapManager.js
@@ -3,66 +3,93 @@ function MapManager($http, $q, $log, $rootScope, $location, $compile,
                     stAnnotationsStore, stEditableLayerBuilder, TimeControlsManager,
                     EditableStoryMap, stStoryMapBaseBuilder, stateSvc,
                     stEditableStoryMapBuilder) {
-    this.storyMap = new EditableStoryMap({target: 'map'});
-    window.storyMap = this.storyMap;
-    StoryPinLayerManager.map = this.storyMap;
+    var svc = {};
+    svc.storyMap = new EditableStoryMap({target: 'map'});
+    window.storyMap = svc.storyMap;
+    StoryPinLayerManager.map = svc.storyMap;
     //StoryBoxLayerManager.map = this.storyMap;
-    this._config = {};
-    this.title = "";
-    this.owner = "";
-    this.storyChapter = 1;
-    this.chapterCount = 1;
-    var self = this;
-    StoryPinLayerManager.storyPinsLayer = this.storyMap.storyPinsLayer;
+    svc._config = {};
+    svc.title = "";
+    svc.owner = "";
+    svc.storyChapter = 1;
+    svc.chapterCount = 1;
+    StoryPinLayerManager.storyPinsLayer = svc.storyMap.storyPinsLayer;
 
-    this.loadMap = function(options) {
-        options = options || {};
-        if (options.id) {
-            stStoryMapBuilder.modifyStoryMap(self.storyMap, options);
-
-            var annotationsLoad = $http.get("/maps/" + options.id + "/annotations");
-            var boxesLoad = $http.get("/maps/" + options.id + "/boxes");
-            $q.all([annotationsLoad, boxesLoad]).then(function(values) {
-                StoryPinLayerManager.loadFromGeoJSON(values[0].data, self.storyMap.getMap().getView().getProjection(), true);
-            });
-        } else if (options.url) {
-            var mapLoad = $http.get(options.url).then(function(response) {
-                stEditableStoryMapBuilder.modifyStoryMap(self.storyMap, response.data);
-            }).catch(function(data, status) {
-                if (status === 401) {
-                    window.console.warn('Not authorized to see map ' + mapId);
-                    stStoryMapBaseBuilder.defaultMap(self.storyMap);
-                }
-            });
-            var annotationsURL = options.url.replace('/data','/annotations');
-            if (annotationsURL.slice(-1) === '/') {
-                annotationsURL = annotationsURL.slice(0, -1);
-            }
-            var annotationsLoad = $http.get(annotationsURL);
-            $q.all([mapLoad, annotationsLoad]).then(function(values) {
-                var geojson = values[1].data;
-                StoryPinLayerManager.loadFromGeoJSON(geojson, self.storyMap.getMap().getView().getProjection());
-            });
-        } else {
-            stStoryMapBaseBuilder.defaultMap(this.storyMap);
+    svc.getDataFromLocalServer = function(mapId, dataType) {
+      return $http.get("/maps/" + mapId + "/" + dataType)
+      .then(function(response) {
+        console.log('resp');
+        return response;
+      })
+      .catch(function(data, status) {
+        if (status === 404) {
+          console.log('404 error');
+          return 'error';
         }
-        this.currentMapOptions = options;
+        return 'error';
+      });
     };
 
-    this.initMapLoad = function() {
+    svc.loadMapFromID = function(options) {
+      stStoryMapBuilder.modifyStoryMap(svc.storyMap, options);
+      var annotationsLoad = svc.getDataFromLocalServer(options.id, 'annotations');
+      var boxesLoad = svc.getDataFromLocalServer(options.id, 'boxes');
+      $q.all([annotationsLoad, boxesLoad]).then(function(values) {
+        if (values[0] !== 'error') {
+          StoryPinLayerManager.loadFromGeoJSON(values[0].data,
+          svc.storyMap.getMap().getView().getProjection(), true);
+        }
+      });
+    };
+
+    svc.loadMapFromUrl = function(options) {
+      var mapLoad = $http.get(options.url).then(function(response) {
+        stEditableStoryMapBuilder.modifyStoryMap(svc.storyMap,
+                                                 response.data);
+      }).catch(function(data, status) {
+        if (status === 401) {
+            window.console.warn('Not authorized to see map ' + mapId);
+            stStoryMapBaseBuilder.defaultMap(svc.storyMap);
+        }
+      });
+      var annotationsURL = options.url.replace('/data','/annotations');
+      if (annotationsURL.slice(-1) === '/') {
+        annotationsURL = annotationsURL.slice(0, -1);
+      }
+      var annotationsLoad = $http.get(annotationsURL);
+      $q.all([mapLoad, annotationsLoad]).then(function(values) {
+        var geojson = values[1].data;
+        StoryPinLayerManager.loadFromGeoJSON(geojson, svc.storyMap.getMap()
+        .getView().getProjection());
+      });
+    };
+
+    svc.loadMap = function(options) {
+      options = options || {};
+      if (options.id) {
+        svc.loadMapFromID(options);
+      } else if (options.url) {
+        svc.loadMapFromUrl(options);
+      } else {
+        stStoryMapBaseBuilder.defaultMap(svc.storyMap);
+      }
+      svc.currentMapOptions = options;
+    };
+
+    svc.initMapLoad = function() {
       var config = stateSvc.getChapterConfig();
       if (!config) { return; }
-      self.title = config.about.title;
-      self.username = config.about.username;
-      self.owner = config.about.owner;
-      self.loadMap(config);
+      svc.title = config.about.title;
+      svc.username = config.about.username;
+      svc.owner = config.about.owner;
+      svc.loadMap(config);
     };
 
     // add locationchange listener to composerCtrl and affect necessary changes in stateSvc
 
-    this.addLayer = function(name, settings, server, fitExtent, styleName, title) {
-        self.storyMap.setAllowZoom(settings.allowZoom);
-        self.storyMap.setAllowPan(settings.allowPan);
+    svc.addLayer = function(name, settings, server, fitExtent, styleName, title) {
+        svc.storyMap.setAllowZoom(settings.allowZoom);
+        svc.storyMap.setAllowPan(settings.allowPan);
         if (fitExtent === undefined) {
             fitExtent = true;
         }
@@ -87,22 +114,24 @@ function MapManager($http, $q, $log, $rootScope, $location, $compile,
             timeEndpoint: server.timeEndpoint ? server.timeEndpoint(name): undefined,
             type: (settings.asVector === true) ? 'VECTOR': 'WMS'
         };
-        return stEditableLayerBuilder.buildEditableLayer(options, self.storyMap.getMap()).then(function(a) {
-            self.storyMap.addStoryLayer(a);
+        return stEditableLayerBuilder.buildEditableLayer(options, svc.storyMap.getMap()).then(function(a) {
+            svc.storyMap.addStoryLayer(a);
             if (fitExtent === true) {
                 a.get('latlonBBOX');
                 var extent = ol.proj.transformExtent(
                       a.get('latlonBBOX'),
                       'EPSG:4326',
-                      self.storyMap.getMap().getView().getProjection()
+                      svc.storyMap.getMap().getView().getProjection()
                 );
                 // prevent getting off the earth
                 extent[1] = Math.max(-20037508.34, Math.min(extent[1], 20037508.34));
                 extent[3] = Math.max(-20037508.34, Math.min(extent[3], 20037508.34));
-                self.storyMap.getMap().getView().fitExtent(extent, self.storyMap.getMap().getSize());
+                svc.storyMap.getMap().getView().fitExtent(extent, svc.storyMap.getMap().getSize());
             }
         });
     };
+
+    return svc;
 }
 
 module.exports = MapManager;
