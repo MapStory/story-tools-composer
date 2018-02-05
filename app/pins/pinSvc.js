1c91d7507b143f715439aacc047c8406231ca9f5
Jose Llausas <jose@zunware.com>
2018-02-05 09:52:31 -0600

    Linted and commented.

--- app/pins/pinSvc.js
+++ app/pins/pinSvc.js
@@ -8,25 +8,40 @@ function pinSvc(
   stateSvc,
   MapManager
 ) {
-  const svc = {};
-  svc.pins = [[]];
-  svc.currentPin = null;
-  svc.isDrawing = false;
+  const svc = {}; // this
+  svc.pins = [[]]; // The collection of pins
+  svc.currentPin = null; // The current Pin being edited
+  svc.isDrawing = false; // Draw Pin Mode flag
+  // For Drag functionality:
+  svc.coordinate = null;
+  svc.cursor = "pointer";
+  svc.feature = null;
+  svc.previousCursor = undefined;
 
+  /**
+   * Creates a new Pin from data
+   * TODO: Check if this actually works as expected
+   * @param data The data to build the pin from.
+   */
   svc.Pin = function(data) {
+    // Angular copydata
     const copyData = angular.copy(data);
+    // Deletes and re-sets geometry
     delete data.geometry;
     ol.Feature.call(this, data);
     this.properties = data;
     this.setGeometry(new ol.geom.Point(copyData.geometry.coordinates));
+    // Sets the time with the time service.
     this.start_time = timeSvc.getTime(this.start_time);
     this.end_time = timeSvc.getTime(this.end_time);
   };
+  // Set the pin's prototype and constructor
   svc.Pin.prototype = Object.create(ol.Feature.prototype);
   svc.Pin.prototype.constructor = svc.Pin;
 
   const embed_width = '"180"';
   const embed_height = '"180"';
+  // The model
   const model_attributes = [
     "title",
     "id",
@@ -40,6 +55,7 @@ function pinSvc(
     "pause_playback",
     "auto_show"
   ];
+  // Filters
   const filterPropertiesFromValidation = [
     "id",
     "_id",
@@ -51,30 +67,51 @@ function pinSvc(
     "auto_show"
   ];
 
+  /**
+   * Adds an empty chapter to the pin list.
+   */
   svc.addChapter = () => {
     svc.pins.push([]);
   };
 
+  /**
+   * Creates a vectorlayer with the storypinlayer metadata.
+   * @returns {*}
+   */
   svc.createStoryPinLayer = () =>
     featureManagerSvc.createVectorLayer(
       featureManagerSvc.storyPinLayerMetadata
     );
 
+  // TODO: Move this to somewhere more appropiate
   svc.pinLayer = svc.createStoryPinLayer();
 
+  /**
+   * Adds an empty pin to the current chapter.
+   */
   svc.addEmptyPinToCurrentChapter = () => {
     svc.pins[stateSvc.getChapter() - 1].push({});
     $rootScope.$broadcast("pin-added", stateSvc.getChapter() - 1);
   };
 
+  /**
+   * Removes a chapter with the given index
+   * @param chapter_index Chapter index to remove.
+   */
   svc.removeChapter = chapter_index => {
     svc.pins.splice(chapter_index, 1);
   };
 
+  /**
+   * Gets the geo-features from the server.
+   * @param config Config
+   * @return Returns a promise
+   */
   svc.getFeaturesFromServer = config => {
+    const features_url = `/maps/${config.id}/annotations`;
     const defer = $q.defer();
     $http({
-      url: `/maps/${config.id}/annotations`,
+      url: features_url,
       method: "GET"
     }).then(result => {
       defer.resolve(result.data);
@@ -82,13 +119,23 @@ function pinSvc(
     return defer.promise;
   };
 
+  /**
+   * Adds pins from a GEOJSON object
+   * @param geojson The object.
+   * @param chapter Chapter to insert to.
+   */
   svc.addPinsFromGeojsonObj = (geojson, chapter) => {
     geojson.features.map(feature => {
       svc.addPinFromGeojsonObj(feature, chapter);
     });
   };
-
+  /**
+   * Adds a single pin from a GEOJSON feature object.
+   * @param feature The feature
+   * @param chapter The chapter to insert into.
+   */
   svc.addPinFromGeojsonObj = (feature, chapter) => {
+    // Convert the data
     const props = feature.properties;
     props.geometry = $.parseJSON(feature.geometry);
     props.geometry.coordinates = ol.proj.transform(
@@ -97,13 +144,19 @@ function pinSvc(
       "EPSG:3857"
     );
     props.id = feature.id;
-    props.start_time = props.start_time * 1000;
-    props.end_time = props.end_time * 1000;
+    props.start_time *= 1000;
+    props.end_time *= 1000;
+
+    // Create the new pin and insert to chapter
     const storyPin = new svc.Pin(props);
     storyPin.setId(feature.id);
     svc.pins[chapter].push(storyPin);
   };
 
+  /**
+   * TODO: Why is this needed?
+   * @param prop
+   */
   svc.addGetterAndSetterToPinPrototype = prop => {
     Object.defineProperty(svc.Pin.prototype, prop, {
       get: function() {
@@ -122,12 +175,23 @@ function pinSvc(
     });
   };
 
+  /**
+   * Get pins from chapter index
+   * @param chapter_index The chapter
+   * @returns {Array|*} An array of Pins
+   */
   svc.getPins = chapter_index => svc.pins[chapter_index] || [];
+
 
   svc.reorderPins = (from_index, to_index) => {
     svc.pins.splice(to_index, 0, svc.pins.splice(from_index, 1)[0]);
   };
 
+  /**
+   * Removes a pin from a chapter
+   * @param storyPin The Storypin to be removed.
+   * @param chapter_index The chapter to remove from.
+   */
   svc.removePin = (storyPin, chapter_index) => {
     for (let i = 0; i < svc.pins[chapter_index].length; i++) {
       if (storyPin.id_ == svc.pins[chapter_index][i].id_) {
@@ -148,6 +212,12 @@ function pinSvc(
     $rootScope.$broadcast("pin-removed", chapter_index);
   };
 
+  /**
+   * Validates pin config.
+   * @param pinInstantiationObj
+   * @param propertyName
+   * @returns {*|boolean}
+   */
   svc.validatePinProperty = (pinInstantiationObj, propertyName) =>
     pinInstantiationObj.hasOwnProperty(propertyName) &&
     (goog.isDefAndNotNull(pinInstantiationObj[propertyName]) &&
@@ -172,6 +242,10 @@ function pinSvc(
     return true;
   };
 
+  /**
+   * Deals with a bad pin.
+   * @param invalidProperties
+   */
   svc.handleInvalidPin = invalidProperties => {
     $translate(invalidProperties).then(translations => {
       let invalid_string =
@@ -186,12 +260,20 @@ function pinSvc(
     });
   };
 
-  svc.addPin = function(props, chapter_index) {
+  /**
+   * Adds a new Pin with the given properties to the given chapter.
+   * @param props The pin's properties.
+   * @param chapter_index The chapter's index.
+   * @returns {boolean} True when OK.
+   */
+  svc.addPin = (props, chapter_index) => {
+    // Check if data is OK
     const pinValidated = svc.validateAllPinProperties(props);
     if (pinValidated !== true) {
       svc.handleInvalidPin(pinValidated);
       return false;
     }
+    // Check time is OK
     if (timeSvc.getTime(props.start_time) > timeSvc.getTime(props.end_time)) {
       console.log("Start Time must be before End Time", "Invalid Time");
       toastr.error("Start Time must be before End Time", "Invalid Time");
@@ -205,15 +287,23 @@ function pinSvc(
         `height=${embed_height}`
       );
     }
+    // Create the new storypin
     const storyPin = new svc.Pin(props);
+    // Push to the chapter
     svc.pins[chapter_index].push(storyPin);
+    // Broadcast event
     $rootScope.$broadcast("pin-added", chapter_index);
     return true;
   };
 
-  // @TODO: write test for this after mapService functions are ported over
+  /**
+   * Updates a pin at the given chapter index
+   * @param pin The Pin
+   * @param chapter_index The chapter
+   * @ TODO: write test for this after mapService functions are ported over
+   */
   svc.updatePin = (pin, chapter_index) => {
-    //Only set new geometry if location was saved on pin object
+    // Only set new geometry if location was saved on pin object
     if (goog.isDefAndNotNull(pin.geometry)) {
       // mapService_.removeDraw();
       // mapService_.removeSelect();
@@ -227,10 +317,11 @@ function pinSvc(
         svc.pins[iPin] = pin;
       }
     }
-    rootScope_.$broadcast("pin-added", chapter_index);
+    // TODO: Check if this broadcast is behaving OK
+    $rootScope.$broadcast("pin-added", chapter_index);
   };
 
-  //@TODO: move to another service
+  //@ TODO: move to another service
   svc.isUrl = str => {
     if (!/^(f|ht)tps?:\/\//i.test(str)) {
       return false;
@@ -238,6 +329,11 @@ function pinSvc(
     return true;
   };
 
+  /**
+   * Sets the default values for a given Pin.
+   * @param pin The Pin
+   * @returns {*} The same pin with values changed.
+   */
   svc.defaultPinValues = pin => {
     Object.keys(pin).forEach((key, index) => {
       if (pin[key] === "") {
@@ -263,6 +359,11 @@ function pinSvc(
     return pin;
   };
 
+  /**
+   * Bulk create StoryPins
+   * @param pinConfigs Pin configurations.
+   * @param chapter_index The chapter index to add them to.
+   */
   svc.bulkPinAdd = (pinConfigs, chapter_index) => {
     let failedToAdd = 0;
     for (let iPin = 0; iPin < pinConfigs.length; iPin += 1) {
@@ -272,18 +373,19 @@ function pinSvc(
       pin.id = new Date().getUTCMilliseconds();
       pin.geometry = {
         coordinates: ol.proj.transform(
-          [Number(pin["longitude"]), Number(pin["latitude"])],
+          [Number(pin.longitude), Number(pin.latitude)],
           "EPSG:4326",
           "EPSG:3857"
         )
       };
-      delete pin["longitude"];
-      delete pin["latitude"];
+      delete pin.longitude;
+      delete pin.latitude;
       if (
         svc.validateAllPinProperties(pin) !== true ||
         timeSvc.getTime(pin.start_time) > timeSvc.getTime(pin.end_time)
       ) {
         failedToAdd += 1;
+        // TODO: This is not OK! Fix!
         continue;
       }
 
@@ -301,6 +403,10 @@ function pinSvc(
     $rootScope.$broadcast("pin-added", chapter_index);
   };
 
+  /**
+   * Convenience method for adding chapters and pins.
+   * @param config The config.
+   */
   svc.addChaptersAndPins = config => {
     const defer = $q.defer();
     angular.forEach(config.chapters, (chapterConfig, index) => {
@@ -314,15 +420,24 @@ function pinSvc(
     return defer.promise;
   };
 
+  /**
+   * Gets Features from server and converts them to pins.
+   * @param chapterConfig The config.
+   * @return {promise} A promise.
+   */
   svc.getFeaturesAndConvertToPins = chapterConfig => {
     const defer = $q.defer();
     svc.getFeaturesFromServer(chapterConfig).then(geojson => {
-      addPinsFromGeojsonObj(geojson);
+      svc.addPinsFromGeojsonObj(geojson);
       defer.resolve(true);
     });
     return defer.promise;
   };
 
+  /**
+   * Initializes the PinService.
+   * TODO: Verify that this is getting called.
+   */
   svc.initPinSvc = () => {
     const defer = $q.defer();
     const config = stateSvc.getConfig();
@@ -334,6 +449,7 @@ function pinSvc(
     return defer.promise;
   };
 
+  // TODO: Move this somewhere more visible.
   svc.addMultipleGettersAndSettersToPinPrototype(model_attributes);
 
   /*
@@ -386,18 +502,11 @@ function pinSvc(
       source: vectorSource
     });
     // TODO: Add layers to map
-    let map = new ol.Map({
+    const map = new ol.Map({
       layers: [vectorLayer]
     });
   };
 
-  /**
-   * Drag functionality for PINS
-   */
-  svc.coordinate = null;
-  svc.cursor = "pointer";
-  svc.feature = null;
-  svc.previousCursor = undefined;
   /**
    * Drag constructor. Inherits from OpenLayers interaction.Pointer
    */
@@ -417,9 +526,7 @@ function pinSvc(
    */
   svc.Drag.prototype.handleDownEvent = evt => {
     const map = evt.map;
-    const feature = map.forEachFeatureAtPixel(evt.pixel, feature_ => {
-      return feature_;
-    });
+    const feature = map.forEachFeatureAtPixel(evt.pixel, feature_ => feature_);
 
     if (feature) {
       svc.coordinate = evt.coordinate;
@@ -446,9 +553,7 @@ function pinSvc(
   svc.Drag.prototype.handleMoveEvent = evt => {
     if (svc.cursor) {
       const map = evt.map;
-      const feature = map.forEachFeatureAtPixel(evt.pixel, feature_ => {
-        return feature_;
-      });
+      const feature = map.forEachFeatureAtPixel(evt.pixel, feature_ => feature_);
 
       const element = evt.map.getTargetElement();
       if (feature) {
@@ -463,19 +568,30 @@ function pinSvc(
     }
   };
 
+  /**
+   * Callback for the mouse up event for placing Storypins.
+   * @return {boolean}
+   */
   svc.Drag.prototype.handleUpEvent = () => {
     svc.coordinate = null;
     svc.feature = null;
     return false;
   };
 
+  /**
+   * turnPinDrawModeOn
+   * -----------------
+   *
+   * Turns Pin Draw Mode. On the user's next click,
+   * a Pin will be placed and the coordinates for currentPin updated.
+   */
   svc.turnPinDrawModeOn = () => {
     svc.isDrawing = !svc.isDrawing;
     // register an event handler for the click event
     const map = MapManager.storyMap.getMap();
     const overlay = new ol.Overlay({
-      element: document.getElementById('overlay'),
-      positioning: 'bottom-center'
+      element: document.getElementById("overlay"),
+      positioning: "bottom-center"
     });
 
     map.on("click", event => {
@@ -496,6 +612,7 @@ function pinSvc(
       }
     });
   };
+
   return svc;
 }
 
