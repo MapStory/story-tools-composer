0e87d3bb53b024c9042d098304bfbb3eea27a6c0
Darin Acosta <darinjacosta@gmail.com>
2018-03-21 17:25:11 -0500

    add chapter info toggle from toc

--- style/style.js
+++ style/style.js
@@ -3,6 +3,7 @@ require("../app/time/style/time.less");
 // require("../app/layers/style/info-box.less");
 require("../app/map/style/map.less");
 require("../app/ui/style/pin-sidebar.less");
+require("../app/ui/style/sidebar.less");
 require("../app/ui/style/style-sidebar.less");
 require("../app/ui/style/add-layer-sidebar.less");
 require("../app/frames/style/frames.less");
