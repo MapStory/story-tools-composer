e0d7316b5711623bfbba982ea174b0920f9f8bf6
Darin <darinjacosta@gmail.com>
2018-03-24 19:16:37 -0500

    Revert "Storypins (#63)"
    This reverts commit 324eb8006792466beeb80b79bede72122913d4f1.

--- app/pins/index.js
+++ app/pins/index.js
@@ -5,12 +5,8 @@ const pinSvc = require("./pinSvc.js");
 const bulkUploadCtrl = require("./bulkUploadController.js");
 const bulkUploadDirective = require("./bulkUpload.directive");
 const bulkUploadModalController = require("./bulkUploadModal.controller");
-const storypinController = require("./storypin.controller");
 
-angular.module("composer")
-  .factory("pinSvc", pinSvc)
-  .controller("bulkUploadCtrl", bulkUploadCtrl)
-  .controller("bulkUploadModalController", bulkUploadModalController)
-  .directive("bulkUploadDirective", bulkUploadDirective)
-  .controller("storypinController", storypinController);
-
+angular.module("composer").factory("pinSvc", pinSvc);
+angular.module("composer").controller("bulkUploadCtrl", bulkUploadCtrl);
+angular.module("composer").controller("bulkUploadModalController", bulkUploadModalController);
+angular.module("composer").directive("bulkUploadDirective", bulkUploadDirective);
