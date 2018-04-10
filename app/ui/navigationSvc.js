ef648613b4e46f0593fbbc1b2f34b075eacba6b7
Jose <zunware@users.noreply.github.com>
2018-04-09 19:39:54 -0500

    Fixes #588 Storypins chapters (#126)
    * Closes #588
    
    * WIP
    
    * Added to .gitignore
    
    * Fixes tests
    
    * Fixes tests?
    
    * Missing [] fixed
    
    * ah...
    
    * WIP again

--- app/ui/navigationSvc.js
+++ app/ui/navigationSvc.js
@@ -2,28 +2,50 @@ function navigationSvc($location, $rootScope, $log, stateSvc, appConfig) {
   const svc = {};
   const values = { annotations: [], boxes: [], data: [] };
 
+  /**
+   * Navigates to next chapter or loops around.
+   * NOTE: Chapter number starts at 1.
+   */
   svc.nextChapter = () => {
-    const nextChapter = Number(stateSvc.getChapter()) + 1;
+    const thisChapter = Number(stateSvc.getChapter());
+    const nextChapter = thisChapter + 1;
     if (nextChapter <= stateSvc.getChapterCount()) {
+      // Go to next
       $log.info("Going to Chapter ", nextChapter);
       $rootScope.$broadcast("updateTimeValues", values);
+      $rootScope.$broadcast("changingChapter", thisChapter -1, nextChapter -1); // (-1 because indeces start at 1)
       $location.path(appConfig.routes.chapter + nextChapter);
     } else {
+      // Go from last to first.
       $location.path("");
+      $rootScope.$broadcast("changingChapter", thisChapter -1, 0);
     }
   };
 
+  /**
+   * Navigates to previous chapter or loops around.
+   * NOTE: Chapter number starts at 1.
+   */
   svc.previousChapter = () => {
-    const previousChapter = Number(stateSvc.getChapter()) - 1;
+    const thisChapter = Number(stateSvc.getChapter());
+    const previousChapter = thisChapter - 1;
     if (previousChapter > 0) {
+      // Go to previous
       $log.info("Going to the Chapter ", previousChapter);
       $rootScope.$broadcast("updateTimeValues", values);
+      $rootScope.$broadcast("changingChapter", thisChapter -1, previousChapter -1); // (-1 because indeces start at 1)
       $location.path(appConfig.routes.chapter + previousChapter);
     } else {
-      $location.path("");
+      // Go from first to last.
+      svc.goToChapter(stateSvc.getChapterCount());
+      $rootScope.$broadcast("changingChapter", thisChapter -1, stateSvc.getChapterCount() -1);
     }
   };
 
+  /**
+   * Goes to given chapter (from 1 to N chapters)
+   * @param number
+   */
   svc.goToChapter = number => {
     if (number > 0) {
       $log.info("Going to the Chapter ", number);
