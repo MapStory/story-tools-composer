6a3f2a0d690d78e41fe407a2bfb452a9e3d997f5
Shawn Kendrick <skendrick@boundlessgeo.com>
2018-04-12 13:51:22 -0500

    iterate through saved chapters & frames

--- app/ui/composerController.js
+++ app/ui/composerController.js
@@ -267,18 +267,19 @@ function composerController(
   };
 
   $rootScope.$on("updateStorypins", (event, chapters) => {
-    // check for multiple chapters
-    for (let f=0; f<chapters[0].storyframes.length; f++) {
-      const coords = JSON.parse(chapters[0].storyframes[f].center);
-      $scope.frameSettings.push({
-          title: chapters[0].storyframes[f].title,
-          startDate: moment.unix(chapters[0].storyframes[f].start_time).format('YYYY-MM-DD'),
-          endDate: moment.unix(chapters[0].storyframes[f].end_time).format('YYYY-MM-DD'),
-          bb1: [coords[0][0], coords[0][1]],
-          bb2: [coords[1][0], coords[1][1]],
-          bb3: [coords[2][0], coords[2][1]],
-          bb4: [coords[3][0], coords[3][1]]
-      });
+    for (let c=0; c<chapters.length; c++) {
+        for (let f=0; f<chapters[c].storyframes.length; f++) {
+            const coords = JSON.parse(chapters[0].storyframes[f].center);
+            $scope.frameSettings.push({
+                title: chapters[c].storyframes[f].title,
+                startDate: moment.unix(chapters[c].storyframes[f].start_time).format('YYYY-MM-DD'),
+                endDate: moment.unix(chapters[c].storyframes[f].end_time).format('YYYY-MM-DD'),
+                bb1: [coords[0][0], coords[0][1]],
+                bb2: [coords[1][0], coords[1][1]],
+                bb3: [coords[2][0], coords[2][1]],
+                bb4: [coords[3][0], coords[3][1]]
+            });
+        }
     }
   });
 
