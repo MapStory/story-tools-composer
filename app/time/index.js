d8ea62a023c80126e53d89748fc74dac111599d1
Jeremiah Cooper <JCooper@jeremiahcooper.com>
2018-05-18 13:23:03 -0400

    Refactor time service removing old outdated code, updating it for ES6 and solving all linting issues.

--- app/time/index.js
+++ app/time/index.js
@@ -1,8 +1,4 @@
-'use strict';
+import { module } from "angular";
+import stateSvc from "./timeSvc.js";
 
-const angular = require('angular');
-const stateSvc = require('./timeSvc.js');
-const datetimepicker = require('./dateTimePickerDirective.js');
-
-angular.module('composer').factory('timeSvc', stateSvc);
-angular.module('composer').directive('datetimepicker', datetimepicker);
+module("composer").factory("timeSvc", stateSvc);
--- app/time/timeSvc.js
+++ app/time/timeSvc.js
@@ -1,32 +1,33 @@
 function timeSvc() {
   const svc = {};
 
-  var parseDate = function(str) {
-    var date;
-    var dateRegEx = /^(-?)(?:(\d{4})(?:-(\d{2})(?:-(\d{2}))?)?)?(?:(?:T(\d{1,2}):(\d{2}):(\d{2}(?:\.\d+)?)(Z|(?:[+-]\d{1,2}(?::(\d{2}))?)))|Z)?$/;
-    var match = str.match(dateRegEx);
-    if (match && (match[2] || match[8])) { // must have at least year or time
-      var year = parseInt(match[2], 10) || 0;
-      if (match[1]){
-        year = year * -1;
+  const parseDate = str => {
+    let date;
+    const dateRegEx = /^(-?)(?:(\d{4})(?:-(\d{2})(?:-(\d{2}))?)?)?(?:(?:T(\d{1,2}):(\d{2}):(\d{2}(?:\.\d+)?)(Z|(?:[+-]\d{1,2}(?::(\d{2}))?)))|Z)?$/;
+    const match = str.match(dateRegEx);
+    if (match && (match[2] || match[8])) {
+      // must have at least year or time
+      let year = parseInt(match[2], 10) || 0;
+      if (match[1]) {
+        year *= -1;
       }
-      var month = (parseInt(match[3], 10) - 1) || 0;
-      var day = parseInt(match[4], 10) || 1;
+      const month = parseInt(match[3], 10) - 1 || 0;
+      const day = parseInt(match[4], 10) || 1;
       date = new Date(Date.UTC(year, month, day));
       // optional time
-      var type = match[8];
+      const type = match[8];
       if (type) {
-        var hours = parseInt(match[5], 10);
-        var minutes = parseInt(match[6], 10);
-        var secFrac = parseFloat(match[7]);
-        var seconds = secFrac | 0;
-        var milliseconds = Math.round(1000 * (secFrac - seconds));
+        const hours = parseInt(match[5], 10);
+        const minutes = parseInt(match[6], 10);
+        const secFrac = parseFloat(match[7]);
+        const seconds = secFrac || 0;
+        const milliseconds = Math.round(1000 * (secFrac - seconds));
         date.setUTCHours(hours, minutes, seconds, milliseconds);
         // check offset
         if (type !== "Z") {
-          var hoursOffset = parseInt(type, 10);
-          var minutesOffset = parseInt(match[9], 10) || 0;
-          var offset = -1000 * (60 * (hoursOffset * 60) + minutesOffset * 60);
+          const hoursOffset = parseInt(type, 10);
+          const minutesOffset = parseInt(match[9], 10) || 0;
+          const offset = -1000 * (60 * (hoursOffset * 60) + minutesOffset * 60);
           date = new Date(date.getTime() + offset);
         }
       }
@@ -37,28 +38,24 @@ function timeSvc() {
   };
 
   svc.getTime = arg => {
-      const type = typeof arg;
-      if (type === 'number') {
-          return arg;
-      }
-      if (arg instanceof Date) {
-          return arg.getTime();
-      }
-      if (type === 'string') {
-          return parseDate(arg).getTime();
-      }
-      /*jshint eqnull:true */
-      if (arg == null) {
-          return null;
-      }
-      if (isRangeLike(arg)) {
-          /*jshint eqnull:true */
-          return getTime(arg.start != null ? arg.start : arg.end);
-      }
-      throw new Error(`cannot call getTime with ${type}, : ${arg}`);
+    const type = typeof arg;
+    if (type === "number") {
+      return arg;
+    }
+    if (arg instanceof Date) {
+      return arg.getTime();
+    }
+    if (type === "string") {
+      return parseDate(arg).getTime();
+    }
+    /* jshint eqnull:true */
+    if (arg == null) {
+      return null;
+    }
+    throw new Error(`cannot call getTime with ${type}, : ${arg}`);
   };
 
   return svc;
 }
 
-module.exports = timeSvc;
+export default timeSvc;
