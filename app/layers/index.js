2157813751dd1899a6c66a156497ad12a2fa517f
Darin Acosta <darinjacosta@gmail.com>
2018-07-12 13:38:01 -0500

    Remove tile progress controller.

--- app/layers/index.js
+++ app/layers/index.js
@@ -4,12 +4,9 @@ import featureManagerSvc from "./featureManagerSvc";
 import layerSvc from "./layerSvc";
 import layerOptionsSvc from "./layerOptionsSvc";
 import layerList from "./layerList";
-import tileProgressController from "./tileProgressController";
 
 module("composer").factory("layerSvc", layerSvc);
 module("composer").factory("layerOptionsSvc", layerOptionsSvc);
 module("composer").directive("addLayers", addLayers);
 module("composer").directive("layerList", layerList);
 module("composer").factory("featureManagerSvc", featureManagerSvc);
-
-module("composer").controller("tileProgressController", tileProgressController);
