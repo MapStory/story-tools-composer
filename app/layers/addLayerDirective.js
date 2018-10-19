56f77b117e51bfc9ee5fbcb92ff4939df82d1042
Shawn Kendrick <shawnkendrick@users.noreply.github.com>
2018-10-19 17:37:00 -0500

    1596: Feature to prevent users from adding invalid layers to the map (#300)
    * Feature to prevent users from adding invalid layers to the map
    
    * add new line to LESS file
    
    * add new line to JS file
    
    * fix linting errors

--- app/layers/addLayerDirective.js
+++ app/layers/addLayerDirective.js
@@ -16,14 +16,30 @@ function addLayers(layerSvc) {
           return titles;
         });
 
-      scope.addLayerFromSearchResults = layerName => {
-        layerSvc.addLayerFromApiResults({
-          searchValue: scope.searchValue,
-          searchObjects
-        });
-      };
+      scope.addLayerFromSearchResults = (layerName) => {
+        const errorDiv = document.getElementById("noResults");
+        const errorMsg = "<i class=\"glyphicon glyphicon-remove\"></i> No matching layers found.";
+
+        if (searchObjects !== undefined && searchObjects.length !== 0) {
+          for (let x=0; x<searchObjects.length; x++) {
+            if (searchObjects[x].title === layerName) {
+              errorDiv.innerHTML = "";
+              layerSvc.addLayerFromApiResults({
+                searchValue: scope.searchValue,
+                searchObjects
+              });
+              break;
+            } else if (searchObjects[x].title !== layerName) {
+              errorDiv.innerHTML = errorMsg;
+            }
+          }
+        } else {
+          errorDiv.innerHTML = errorMsg;
+        }
+        document.getElementById("layerName").reset();
+      }
     }
-  };
-}
+  }
+};
 
 export default addLayers;
