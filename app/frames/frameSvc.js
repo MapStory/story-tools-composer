ff9f3073fbdf9ebf06c0461e02b16794d5c7000b
Shawn Kendrick <shawnkendrick@users.noreply.github.com>
2019-01-10 10:37:55 -0600

    341: Rearchitect StoryFrames logic  (#383)
    * Add functional utils and begin migration to frame service.
    
    * Get frame settings from frame service.
    
    * Move frame handler to svc.
    
    * migrating frames logic to frameSvc
    
    * fix frame playback
    
    * fix scope and frame playback issues
    
    * resolve playback issues

--- app/frames/frameSvc.js
+++ app/frames/frameSvc.js
@@ -1,8 +1,22 @@
 import moment from "moment";
 import PubSub from "pubsub-js";
 
-function frameSvc(stateSvc) {
+function frameSvc(stateSvc, pinSvc, MapManager, $rootScope) {
   const svc = {};
+  svc.mapManager = MapManager;
+  const map = MapManager.storyMap.getMap();
+  svc.frameSettings = [];
+  svc.copiedFrameSettings = [];
+  svc.currentFrame = 0;
+
+  window.storypinCallback = data => {
+    // Updates StoryPins
+    svc.updateStorypinTimeline(data);
+    // Updates StoryFrames
+    if (svc.copiedFrameSettings) {
+      svc.getCurrentFrame(data);
+    }
+  };
 
   PubSub.subscribe("updateStoryframes", (event, chapters) => {
     stateSvc.config.storyframes = [];
@@ -29,9 +43,213 @@ function frameSvc(stateSvc) {
     }
     svc.storyFrames = stateSvc.config.storyframes;
   });
+
+  const updateStoryframesHandler = () => {
+    $rootScope.$apply(() => {
+      svc.currentFrame = 0;
+      let fetchedFrameSettings = svc.get("storyFrames");
+
+      if (fetchedFrameSettings) {
+        fetchedFrameSettings = fetchedFrameSettings.filter(
+          f => f.chapter === stateSvc.getChapterIndex()
+        );
+      }
+      const fetchedFrames = [];
+
+      for (let i = 0; i < fetchedFrameSettings.length; i++) {
+        if (
+          fetchedFrameSettings[i].startDate &&
+          fetchedFrameSettings[i].endDate
+        ) {
+          fetchedFrames[i] = {
+            id: fetchedFrameSettings[i].id,
+            chapter: fetchedFrameSettings[i].chapter,
+            title: fetchedFrameSettings[i].title,
+            startDate: new Date(
+              moment.utc(fetchedFrameSettings[i].startDate).format("l LT")
+            ),
+            startTime: fetchedFrameSettings[i].startTime,
+            endDate: new Date(
+              moment.utc(fetchedFrameSettings[i].endDate).format("l LT")
+            ),
+            endTime: fetchedFrameSettings[i].endTime,
+            bb1: [
+              fetchedFrameSettings[i].bb1[0],
+              fetchedFrameSettings[i].bb1[1]
+            ],
+            bb2: [
+              fetchedFrameSettings[i].bb2[0],
+              fetchedFrameSettings[i].bb2[1]
+            ],
+            bb3: [
+              fetchedFrameSettings[i].bb3[0],
+              fetchedFrameSettings[i].bb3[1]
+            ],
+            bb4: [
+              fetchedFrameSettings[i].bb4[0],
+              fetchedFrameSettings[i].bb4[1]
+            ]
+          };
+        }
+      }
+      svc.frameSettings = fetchedFrames;
+      svc.copiedFrameSettings = angular.copy(svc.frameSettings);
+    });
+  };
+
+  PubSub.subscribe("updateStoryframes", updateStoryframesHandler);
+  PubSub.subscribe("changingChapter", updateStoryframesHandler);
+
+  svc.getCurrentFrame = date => {
+    const frame = svc.copiedFrameSettings.filter(
+      f => f.chapter === stateSvc.getChapterIndex()
+    )[svc.currentFrame];
+
+    if (frame) {
+      const start = frame.startDate;
+      const end = frame.endDate;
+      svc.checkTimes(date, start, end);
+    }
+  };
+
+  /**
+   * Updates the Storypins on timeline.
+   * Loops the current chapter's StoryPins and determines if they should be shown or hidden.
+   * @param date The date for the layer.
+   */
+  svc.updateStorypinTimeline = date => {
+    // TODO: Use pre-cooked timeframe objects to optimize this?
+    let pinArray = pinSvc.getCurrentPins();
+    // This should not be null. Why is this happening?
+    if (!pinArray) {
+      pinArray = [];
+    }
+    pinArray.forEach(pin => {
+      const startDate = svc.formatDates(pin.startTime);
+      const endDate = svc.formatDates(pin.endTime);
+      const storyLayerStartDate = svc.formatDates(date);
+
+      const shouldShow =
+        moment(storyLayerStartDate).isSameOrAfter(startDate) &&
+        !moment(storyLayerStartDate).isAfter(endDate);
+
+      if (shouldShow) {
+        pin.show();
+      } else {
+        pin.hide();
+      }
+    });
+  };
+
+  svc.checkTimes = (date, start, end) => {
+    if (
+      moment(date).isSameOrAfter(start) &&
+      moment(date).isSameOrBefore(end) &&
+      svc.zoomedIn === false
+    ) {
+      svc.zoomToExtent();
+    } else if (moment(date).isAfter(end) && svc.zoomedIn === true) {
+      svc.zoomOutExtent();
+      if (svc.currentFrame <= svc.copiedFrameSettings.length) {
+        svc.currentFrame += 1;
+        if (svc.currentFrame === svc.copiedFrameSettings.length) {
+          svc.currentFrame = 0;
+        }
+      }
+    } else if (moment(date).isBefore(start) || moment(date).isAfter(end)) {
+      svc.clearBB();
+    }
+  };
+
+  svc.formatDates = date => {
+    const preFormatDate = moment(date);
+    return preFormatDate.format("YYYY-MM-DD");
+  };
+
+  svc.zoomToExtent = () => {
+    let polygon;
+
+    if (svc.copiedFrameSettings[svc.currentFrame]) {
+      polygon = new ol.Feature(
+        new ol.geom.Polygon([
+          [
+            svc.copiedFrameSettings[svc.currentFrame].bb1,
+            svc.copiedFrameSettings[svc.currentFrame].bb2,
+            svc.copiedFrameSettings[svc.currentFrame].bb3,
+            svc.copiedFrameSettings[svc.currentFrame].bb4
+          ]
+        ])
+      );
+    } else if (!svc.copiedFrameSettings[svc.currentFrame]) {
+      for (let i = 0; i < svc.copiedFrameSettings.length; i++) {
+        polygon = new ol.Feature(
+          new ol.geom.Polygon([
+            [
+              svc.copiedFrameSettings[i].bb1,
+              svc.copiedFrameSettings[i].bb2,
+              svc.copiedFrameSettings[i].bb3,
+              svc.copiedFrameSettings[i].bb4
+            ]
+          ])
+        );
+      }
+    }
+
+    const vector = new ol.layer.Vector({
+      source: new ol.source.Vector({
+        features: [polygon]
+      })
+    });
+
+    map.beforeRender(
+      ol.animation.pan({
+        source: map.getView().getCenter(),
+        duration: 1000
+      }),
+      ol.animation.zoom({
+        resolution: map.getView().getResolution(),
+        duration: 1000,
+        easing: ol.easing.easeIn
+      })
+    );
+    svc.zoomedIn = true;
+    vector.set("name", "boundingBox");
+    map.getView().fit(polygon.getGeometry(), map.getSize());
+  };
+
+  svc.zoomOutExtent = () => {
+    const extent = ol.extent.createEmpty();
+    svc.mapManager.storyMap.getStoryLayers().forEach(layer => {
+      ol.extent.extend(extent, layer.get("extent"));
+    });
+
+    const zoom = ol.animation.zoom({
+      resolution: map.getView().getResolution(),
+      duration: 1000,
+      easing: ol.easing.easeOut
+    });
+    map.beforeRender(zoom);
+
+    if (svc.mapManager.storyMap.getStoryLayers().length > 0) {
+      map.getView().fit(extent, map.getSize());
+    } else {
+      map.getView().setZoom(3);
+    }
+
+    svc.zoomedIn = false;
+    svc.clearBB();
+  };
+
+  svc.clearBB = () => {
+    map.getLayers().forEach(layer => {
+      if (layer.get("name") === "boundingBox") {
+        map.removeLayer(layer);
+      }
+    });
+  };
+
   svc.get = prop => svc[prop];
   return svc;
 }
 
 export default frameSvc;
-
--- app/ui/composerController.js
+++ app/ui/composerController.js
@@ -1,6 +1,4 @@
-import moment from "moment";
 import PubSub from "pubsub-js";
-
 
 function composerController(
   $scope,
@@ -21,6 +19,7 @@ function composerController(
   stateSvc
 ) {
   let lastSelectedTab = null;
+  $scope.frameSvc = frameSvc;
   $scope.mapManager = MapManager;
   $scope.stateSvc = stateSvc;
   $scope.pinSvc = pinSvc;
@@ -29,62 +28,8 @@ function composerController(
   $scope.selected = { toc: true };
   $scope.viewerMode = $location.search().viewer;
   $scope.showForm = null;
-  $scope.frameSettings = [];
-  $scope.copiedFrameSettings = [];
-  $scope.currentFrame = 0;
-  $scope.zoomedIn = false;
+  $scope.frameSvc.zoomedIn = false;
   let queryLayerLoaded = false;
-  const map = MapManager.storyMap.getMap();
-
-  const updateStoryframesHandler = () => {
-    $scope.$apply(() => {
-      $scope.currentFrame = 0;
-      let fetchedFrameSettings = frameSvc.get("storyFrames");
-
-      if (fetchedFrameSettings) {
-        fetchedFrameSettings = fetchedFrameSettings.filter(f => f.chapter === stateSvc.getChapterIndex());
-      }
-      const fetchedFrames = [];
-
-      for (let i = 0; i < fetchedFrameSettings.length; i++) {
-        if (
-          fetchedFrameSettings[i].startDate &&
-          fetchedFrameSettings[i].endDate
-        ) {
-          fetchedFrames[i] = {
-            id: fetchedFrameSettings[i].id,
-            chapter: fetchedFrameSettings[i].chapter,
-            title: fetchedFrameSettings[i].title,
-            startDate: new Date(fetchedFrameSettings[i].startDate),
-            startTime: fetchedFrameSettings[i].startTime,
-            endDate: new Date(fetchedFrameSettings[i].endDate),
-            endTime: fetchedFrameSettings[i].endTime,
-            bb1: [
-              fetchedFrameSettings[i].bb1[0],
-              fetchedFrameSettings[i].bb1[1]
-            ],
-            bb2: [
-              fetchedFrameSettings[i].bb2[0],
-              fetchedFrameSettings[i].bb2[1]
-            ],
-            bb3: [
-              fetchedFrameSettings[i].bb3[0],
-              fetchedFrameSettings[i].bb3[1]
-            ],
-            bb4: [
-              fetchedFrameSettings[i].bb4[0],
-              fetchedFrameSettings[i].bb4[1]
-            ]
-          };
-        }
-      }
-      $scope.frameSettings = fetchedFrames;
-      $scope.copiedFrameSettings = angular.copy($scope.frameSettings);
-    })
-  };
-
-  PubSub.subscribe("updateStoryframes", updateStoryframesHandler);
-  PubSub.subscribe("changingChapter", updateStoryframesHandler);
 
   $scope.layerViewerMode = window.mapstory.layerViewerMode;
 
@@ -135,16 +80,17 @@ function composerController(
 
   $scope.stateSvc.previousChapter = $scope.stateSvc.getChapter();
 
-  const loadMap = (event) => {
+  const loadMap = event => {
     const urlChapterId = $location.path().split("chapter/")[1];
     const chapterCount = stateSvc.getChapterCount();
     if (urlChapterId > chapterCount) {
       $scope.navigationSvc.goToChapter(1);
     }
     if (event) {
-      PubSub.publish("changingChapter",
-        {currentChapterIndex: $scope.stateSvc.getChapter() - 1,
-          previousChapterIndex: $scope.stateSvc.previousChapter - 1})
+      PubSub.publish("changingChapter", {
+        currentChapterIndex: $scope.stateSvc.getChapter() - 1,
+        previousChapterIndex: $scope.stateSvc.previousChapter - 1
+      });
     }
     $scope.mapManager.initMapLoad();
     $scope.stateSvc.updateCurrentChapterConfig();
@@ -204,8 +150,8 @@ function composerController(
   };
 
   $scope.saveMap = () => {
-    pinSvc.onStoryPinSave();
     stateSvc.save();
+    pinSvc.onStoryPinSave();
   };
 
   $scope.publishMap = () => {
@@ -237,7 +183,7 @@ function composerController(
   };
 
   PubSub.subscribe("chapterCreated", (event, index) => {
-    $scope.updateSelected("info", index)
+    $scope.updateSelected("info", index);
   });
 
   $scope.nextChapter = navigationSvc.nextChapter;
@@ -264,165 +210,6 @@ function composerController(
       uibmodalInstance.dismiss("close");
     };
   };
-
-  $scope.formatDates = date => {
-    const preFormatDate = moment(date);
-    return preFormatDate.format("YYYY-MM-DD");
-  };
-
-  /**
-   * Updates the Storypins on timeline.
-   * Loops the current chapter's StoryPins and determines if they should be shown or hidden.
-   * @param date The date for the layer.
-   */
-  $scope.updateStorypinTimeline = date => {
-    // TODO: Use pre-cooked timeframe objects to optimize this?
-    let pinArray = pinSvc.getCurrentPins();
-    // This should not be null. Why is this happening?
-    if (!pinArray) {
-      pinArray = [];
-    }
-    pinArray.forEach(pin => {
-      const startDate = $scope.formatDates(pin.startTime);
-      const endDate = $scope.formatDates(pin.endTime);
-      const storyLayerStartDate = $scope.formatDates(date);
-
-      const shouldShow = moment(storyLayerStartDate).isSameOrAfter(startDate) && !moment(storyLayerStartDate).isAfter(endDate);
-
-      if (shouldShow) {
-        pin.show();
-      } else {
-        pin.hide();
-      }
-    });
-  };
-
-  /**
-   * Callback for timeline update.
-   * @param data Data from the timeline.
-   */
-  window.storypinCallback = data => {
-    // Updates StoryPins.
-    $scope.updateStorypinTimeline(data);
-    // Updates StoryFrames
-    if ($scope.copiedFrameSettings) {
-      $scope.getCurrentFrame(data);
-    }
-  };
-
-  $scope.getCurrentFrame = date => {
-    const frame = $scope.copiedFrameSettings.filter(f => f.chapter === stateSvc.getChapterIndex())[$scope.currentFrame];
-    if (frame) {
-      const start = frame.startDate;
-      const end = frame.endDate;
-      $scope.checkTimes(date, start, end);
-    }
-  };
-
-
-
-  $scope.checkTimes = (date, start, end) => {
-    if (moment(date).isSameOrAfter(start) && moment(date).isSameOrBefore(end) && $scope.zoomedIn === false) {
-      $scope.zoomToExtent();
-    } else if (moment(date).isAfter(end) && $scope.zoomedIn === true) {
-      $scope.zoomOutExtent();
-      if ($scope.currentFrame <= $scope.copiedFrameSettings.length) {
-        $scope.currentFrame += 1;
-        if ($scope.currentFrame === $scope.copiedFrameSettings.length) {
-          $scope.currentFrame = 0;
-        }
-      }
-    } else if (moment(date).isBefore(start) || moment(date).isAfter(end)) {
-      $scope.clearBB();
-    }
-  };
-
-  $scope.zoomToExtent = () => {
-    let polygon;
-
-    if ($scope.copiedFrameSettings[$scope.currentFrame]) {
-      polygon = new ol.Feature(
-        new ol.geom.Polygon([
-          [
-            $scope.copiedFrameSettings[$scope.currentFrame].bb1,
-            $scope.copiedFrameSettings[$scope.currentFrame].bb2,
-            $scope.copiedFrameSettings[$scope.currentFrame].bb3,
-            $scope.copiedFrameSettings[$scope.currentFrame].bb4
-          ]
-        ])
-      )
-    }
-    else if (!$scope.copiedFrameSettings[$scope.currentFrame]) {
-      for (let i=0; i < $scope.copiedFrameSettings.length; i ++) {
-        polygon = new ol.Feature(
-          new ol.geom.Polygon([
-            [
-              $scope.copiedFrameSettings[i].bb1,
-              $scope.copiedFrameSettings[i].bb2,
-              $scope.copiedFrameSettings[i].bb3,
-              $scope.copiedFrameSettings[i].bb4
-            ]
-          ])
-        )
-      }
-    }
-
-    const vector = new ol.layer.Vector({
-      source: new ol.source.Vector({
-        features: [polygon]
-      })
-    });
-
-    map.beforeRender(
-      ol.animation.pan({
-        source: map.getView().getCenter(),
-        duration: 1000
-      }),
-      ol.animation.zoom({
-        resolution: map.getView().getResolution(),
-        duration: 1000,
-        easing: ol.easing.easeIn
-      })
-    );
-    $scope.zoomedIn = true;
-    vector.set("name", "boundingBox");
-    map.getView().fit(polygon.getGeometry(), map.getSize());
-  };
-
-  $scope.zoomOutExtent = () => {
-    const extent = ol.extent.createEmpty();
-    $scope.mapManager.storyMap.getStoryLayers().forEach(layer => {
-      ol.extent.extend(extent, layer.get("extent"));
-    });
-
-    const zoom = ol.animation.zoom({
-      resolution: map.getView().getResolution(),
-      duration: 1000,
-      easing: ol.easing.easeOut
-    });
-    map.beforeRender(zoom);
-
-    if ($scope.mapManager.storyMap.getStoryLayers().length > 0) {
-      map.getView().fit(extent, map.getSize());
-    } else {
-      map.getView().setZoom(3);
-    }
-
-    $scope.zoomedIn = false;
-    $scope.clearBB();
-  };
-
-  $scope.clearBB = () => {
-    map.getLayers().forEach(layer => {
-      if (layer.get("name") === "boundingBox") {
-        map.removeLayer(layer);
-      }
-    });
-  }
 }
 
 module.exports = composerController;
-
-
-
-
